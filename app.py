dd36ff74431a5321ef42c3767b0899852e605f3f
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-09 11:35:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,43 @@
 import openai
 import streamlit as st
-from streamlit_chat import message
-import os 
-from dotenv import load_dotenv
+from streamlit_pills import pills
 
-load_dotenv('api_key.env')
-openai.api_key = os.environ.get('API_KEY')
+openai.api_key = st.secrets['api_secret']
 
-def generate_response(prompt):
-    completion = openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.6,
-    )
-    message = completion.choices[0].text
-    return message
+st.subheader("AI Assistant : Streamlit + OpenAI: `stream` *argument*")
 
-def main():
-    st.title("ChatGPT-like Web App")
-    
-    # Storing the chat
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = []
-    if 'past' not in st.session_state:
-        st.session_state['past'] = []
-    
-    user_input = st.text_input("You:", key='input')
-    
-    if user_input:
-        output = generate_response(user_input)
-        # Store the output
-        st.session_state['past'].append(user_input)
-        st.session_state['generated'].append(output)
-    
-    if st.session_state['generated']:
-        for i in range(len(st.session_state['generated'])-1, -1, -1):
-            message(st.session_state["generated"][i], key=str(i))
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+# You can also use radio buttons instead
+selected = pills("", ["NO Streaming", "Streaming"], ["🎈", "🌈"])
 
-if __name__ == "__main__":
-    main()
+user_input = st.text_input("You: ",placeholder = "Ask me anything ...", key="input")
+
+if st.button("Submit", type="primary"):
+    st.markdown("----")
+    res_box = st.empty()
+    
+    if selected == "Streaming":
+        report = []
+        # Looping over the response
+        for resp in openai.Completion.create(model='text-davinci-003',
+                                            prompt=user_input,
+                                            max_tokens=120, 
+                                            temperature = 0.5,
+                                            stream = True):
+            # join method to concatenate the elements of the list 
+            # into a single string, 
+            # then strip out any empty strings
+            report.append(resp.choices[0].text)
+            result = "".join(report).strip()
+            result = result.replace("\n", "")        
+            res_box.markdown(f'*{result}*') 
+            
+    else:
+        completions = openai.Completion.create(model='text-davinci-003',
+                                            prompt=user_input,
+                                            max_tokens=120, 
+                                            temperature = 0.5,
+                                            stream = False)
+        result = completions.choices[0].text
+        
+        res_box.write(result)
+st.markdown("----")
